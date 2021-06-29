476c1f6c2adc2ea632c1653926af5abe96afbb58
Barbara Góral <barbara.zofia.goral@gmail.com>
2021-06-29 12:43:05 +0200

    Add frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Frontend works!</p>
+        <button type="button" class="btn btn-warning">
+          Warning
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
