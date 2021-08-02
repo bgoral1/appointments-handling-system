31e31f0dbf2e9ee8dc23d893df5c1b0181eb62f5
Barbara Góral <barbara.zofia.goral@gmail.com>
2021-08-02 18:46:23 +0200

    Implment contexts in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage';
 import PanelPage from './pages/PanelPage';
 import AuthContext from './context/auth-context';
+import { AppointmentsContextProvider } from './context/appointments-context';
+import { ServicesContextProvider } from './context/services-context';
+import { DentistsContextProvider } from './context/dentists-context';
+import { PatientsContextProvider } from './context/patients-context';
 
 const App = () => {
   const [user, setUser] = useState({
@@ -31,26 +35,36 @@ const App = () => {
             logout: logout,
           }}
         >
-          <main>
-            <Switch>
-              {user.token && <Redirect from="/login" to="/panel" />}
-              {!user.token && <Redirect from="/panel" to="/login" />}
-              <Route path="/" exact component={HomePage} />
-              {!user.token && <Route path="/login" component={LoginPage} />}
-              {user.token && (
-                <Route path="/panel" exact component={PanelPage} />
-              )}
-              {user.token && (
-                <Route path="/panel/uslugi" component={PanelPage} />
-              )}
-              {user.token && (
-                <Route path="/panel/dentysci" component={PanelPage} />
-              )}
-              {user.token && (
-                <Route path="/panel/pacjenci" component={PanelPage} />
-              )}
-            </Switch>
-          </main>
+          <AppointmentsContextProvider>
+            <ServicesContextProvider>
+              <DentistsContextProvider>
+                <PatientsContextProvider>
+                  <main>
+                    <Switch>
+                      {user.token && <Redirect from="/login" to="/panel" />}
+                      {!user.token && <Redirect from="/panel" to="/login" />}
+                      <Route path="/" exact component={HomePage} />
+                      {!user.token && (
+                        <Route path="/login" component={LoginPage} />
+                      )}
+                      {user.token && (
+                        <Route path="/panel" exact component={PanelPage} />
+                      )}
+                      {user.token && (
+                        <Route path="/panel/uslugi" component={PanelPage} />
+                      )}
+                      {user.token && (
+                        <Route path="/panel/dentysci" component={PanelPage} />
+                      )}
+                      {user.token && (
+                        <Route path="/panel/pacjenci" component={PanelPage} />
+                      )}
+                    </Switch>
+                  </main>
+                </PatientsContextProvider>
+              </DentistsContextProvider>
+            </ServicesContextProvider>
+          </AppointmentsContextProvider>
         </AuthContext.Provider>
       </>
     </BrowserRouter>
